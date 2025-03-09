e816004d6f7312f34a973274af80fe821dd26330
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-03-09 20:13:52 +0500

    AVO-2-theme: add material3 generated theme files to theme module

---
+++ theme/src/main/java/ru/projects/avocabo/theme/Type.kt
@@ -0,0 +1,30 @@
+package ru.projects.avocabo.theme
+
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
+import androidx.compose.ui.text.style.LineHeightStyle
+
+private val poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold),
+    Font(R.font.poppins_extra_bold),
+    Font(R.font.poppins_medium),
+    Font(R.font.poppins_thin),
+    Font(R.font.poppins_italic),
+    Font(R.font.poppins_light),
+    Font(R.font.poppins_black),
+)
+val defaultTextStyLe = TextStyle(
+    fontFamily = poppins,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None,
+    ),
+)
+
